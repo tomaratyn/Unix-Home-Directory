8e2ee5ffd3ad95c85ad2fb2d27d6683c791525a4
Tom Aratyn <tom@aratyn.name>
2012-06-25 00:23:43 -0400

    Added better emacs setup

---
+++ .emacs.d/elpa/ipython-2927/ipython-autoloads.el
@@ -0,0 +1,18 @@
+;;; ipython-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+
+
+;;;### (autoloads nil nil ("ipython-pkg.el" "ipython.el") (20444
+;;;;;;  59752 910706))
+
+;;;***
+
+(provide 'ipython-autoloads)
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; coding: utf-8
+;; End:
+;;; ipython-autoloads.el ends here
---
+++ .emacs.d/init.el
@@ -0,0 +1,36 @@
+(require 'package)
+(add-to-list 'package-archives '("marmalade" . "http://marmalade-repo.org/packages/") t)
+(package-initialize)
+
+(when (not package-archive-contents)
+  (package-refresh-contents))
+
+;; Add in your own as you wish:
+(defvar my-packages '(starter-kit color-theme solarized-theme markdown-mode)
+  "A list of packages to ensure are installed at launch.")
+
+(dolist (p my-packages)
+  (when (not (package-installed-p p))
+    (package-install p)))
+
+(require 'auto-complete-config)
+
+(set-default-font "-apple-Monaco-medium-normal-normal-*-13-*-*-*-m-0-iso10646-1")
+
+(load-theme 'solarized-light t)
+
+(setq python-indent 4)
+(setq js-indent-level 2)
+
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(custom-safe-themes (quote ("d2622a2a2966905a5237b54f35996ca6fda2f79a9253d44793cfe31079e3c92b" "501caa208affa1145ccbb4b74b6cd66c3091e41c5bb66c677feda9def5eab19c" default))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
