f61dcf29c484d6097c6200e76dc8e0ff2684f3bf
Tom Aratyn <tom@aratyn.name>
2012-09-04 00:58:16 -0400

    some updated settins (git, emacs, vim)

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -14,13 +14,16 @@
     (package-install p)))
 
 (require 'auto-complete-config)
+(ac-config-default)
 
 (set-default-font "-apple-Monaco-medium-normal-normal-*-13-*-*-*-m-0-iso10646-1")
-
 (load-theme 'solarized-light t)
+
+(global-linum-mode 1)
 
 (setq python-indent 4)
 (setq js-indent-level 2)
+(setq html-indent-level 2)
 
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
