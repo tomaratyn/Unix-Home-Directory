2c283d35b711fafa0bea427f2c041ef28e3fc00f
Tom Aratyn <tom@aratyn.name>
2013-06-03 20:28:02 -0400

    web-mode hooks work

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -31,15 +31,15 @@
             (setq sgml-basic-offset 4)))
 
 
-; (require 'web-mode)
-; (add-to-list 'auto-mode-alist '("\\.phtml\\'" . web-mode))
-; (add-to-list 'auto-mode-alist '("\\.html\\'" . web-mode))
-; (add-to-list 'auto-mode-alist '("\\.tpl\\.php\\'" . web-mode))
-; (add-to-list 'auto-mode-alist '("\\.jsp\\'" . web-mode))
-; (add-to-list 'auto-mode-alist '("\\.as[cp]x\\'" . web-mode))
-; (add-to-list 'auto-mode-alist '("\\.erb\\'" . web-mode))
-; (add-to-list 'auto-mode-alist '("\\.mustache\\'" . web-mode))
-; (add-to-list 'auto-mode-alist '("\\.djhtml\\'" . web-mode))
+(require 'web-mode)
+(add-to-list 'auto-mode-alist '("\\.phtml\\'" . web-mode))
+(add-to-list 'auto-mode-alist '("\\.html\\'" . web-mode))
+(add-to-list 'auto-mode-alist '("\\.tpl\\.php\\'" . web-mode))
+(add-to-list 'auto-mode-alist '("\\.jsp\\'" . web-mode))
+(add-to-list 'auto-mode-alist '("\\.as[cp]x\\'" . web-mode))
+(add-to-list 'auto-mode-alist '("\\.erb\\'" . web-mode))
+(add-to-list 'auto-mode-alist '("\\.mustache\\'" . web-mode))
+(add-to-list 'auto-mode-alist '("\\.djhtml\\'" . web-mode))
 
 
 (require 'auto-complete-config)
